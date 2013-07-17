10eea395f61c744d6762fba74fe67ccddf69675a
Samson Chan <samson@strumsoft.com>
2013-07-17 15:35:16 -0700

    fixed missing import

--- client/TVNight/src/com/hackathon/tvnight/api/GetShowList.java
+++ client/TVNight/src/com/hackathon/tvnight/api/GetShowList.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
