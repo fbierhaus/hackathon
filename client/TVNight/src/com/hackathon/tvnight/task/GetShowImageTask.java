f8a36f299aef1768eac3ec80302a45d4aa0c22c5
Samson Chan <samson@strumsoft.com>
2013-07-17 13:31:09 -0700

    Modified GetShowImageTask to get images for a list of rovi id, minimum interval between each request is 500ms.

--- client/TVNight/src/com/hackathon/tvnight/task/GetShowImageTask.java
+++ client/TVNight/src/com/hackathon/tvnight/task/GetShowImageTask.java
@@ -3,13 +3,15 @@ package com.hackathon.tvnight.task;
 import java.util.List;
 
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
 import com.hackathon.tvnight.api.GetShowImage;
 import com.hackathon.tvnight.model.ShowImage;
+import com.hackathon.tvnight.model.TVShow;
 
-public class GetShowImageTask extends AsyncTask<String, Void, List<ShowImage>> {
+public class GetShowImageTask extends AsyncTask<String[], Void, Void> {
 	private Handler mHandler;
 	private int mMsgCode;
 	
@@ -38,20 +40,38 @@ public class GetShowImageTask extends AsyncTask<String, Void, List<ShowImage>> {
 	}
 
 	@Override
-	protected List<ShowImage> doInBackground(String... params) {
+	protected Void doInBackground(String[]... params) {
 		List<ShowImage> list = null;
 		if (params.length > 0) {
-			list = (new GetShowImage()).getImageList(params[0]);	// params[0] is roci id
+			String[] idList = params[0];
+			
+			for (String id : idList) {
+				list = (new GetShowImage()).getImageList(id);	// params[0] is roci id
+
+				long starttime = System.currentTimeMillis();
+				
+				// end of task, notify the handler 
+				Message msg = mHandler.obtainMessage(mMsgCode);
+				Bundle data = new Bundle();
+				data.putString(TVShow.ID_KEY_ROVI, id);
+				msg.setData(data);
+				msg.obj = list;
+				msg.sendToTarget();
+				
+				long endtime = System.currentTimeMillis();
+				long elapsed = endtime - starttime;
+				
+				if (elapsed < 500) {
+					try {
+						Thread.sleep(500-elapsed);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}				
+			}
 		}
-		return list;
+		return null;
 	}
 
-	@Override
-	protected void onPostExecute(List<ShowImage> result) {
-		// end of task, notify the handler 
-		Message msg = mHandler.obtainMessage(mMsgCode);
-		msg.obj = result;
-		msg.sendToTarget();
-	}
-	
 }
