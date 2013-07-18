491b3e7f45035e5e7e4dca18afbe6df83a172772
Samson Chan <samson@strumsoft.com>
2013-07-17 19:29:08 -0700

    fixed RestClient2 in client

--- client/TVNight/src/com/vzw/hackathon/apihandler/RestClient2.java
+++ client/TVNight/src/com/vzw/hackathon/apihandler/RestClient2.java
@@ -248,72 +248,7 @@ public class RestClient2 {
 		return id;
 	}
 	
-	
-	public static int sendFling(byte[] data, String fileName, String serverBaseURL){
-		int id = -1;
-		
-		String boundary = Long.toHexString(System.currentTimeMillis()); // Just generate some unique random value.
-		String CRLF = "\r\n"; // Line separator required by multipart/form-data.
-		
-		String url = serverBaseURL + "/server/flings";
-		PrintWriter writer = null;
-		URLConnection connection = null;
-		String charset = "UTF-8";
-		try {
-			connection = new URL(url).openConnection();
-			connection.setDoOutput(true);
-			connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-			
-		    OutputStream output = connection.getOutputStream();
-		    writer = new PrintWriter(new OutputStreamWriter(output, charset), true); // true = autoFlush, important!
 
-		    // Send binary file.
-		    writer.append("--" + boundary).append(CRLF);
-		    writer.append("Content-Disposition: form-data; name=\"fling\"; filename=\"" + fileName + "\"").append(CRLF);
-		    writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(fileName)).append(CRLF);
-		    writer.append("Content-Transfer-Encoding: binary").append(CRLF);
-		    writer.append(CRLF).flush();
-		    InputStream input = null;
-		    try {
-		        input = new ByteArrayInputStream(data);
-		        byte[] buffer = new byte[1024];
-		        for (int length = 0; (length = input.read(buffer)) > 0;) {
-		            output.write(buffer, 0, length);
-		        }
-		        output.flush(); // Important! Output cannot be closed. Close of writer will close output as well.
-		    } finally {
-		        if (input != null) try { input.close(); } catch (IOException logOrIgnore) {}
-		    }
-		    writer.append(CRLF).flush(); // CRLF is important! It indicates end of binary boundary.
-
-		    // End of multipart/form-data.
-		    writer.append("--" + boundary + "--").append(CRLF);
-		    
-		} catch (Exception e) {
-			System.out.println("Error uploading fling" + e);
-		} finally {
-		    if (writer != null) writer.close();
-		}
-		
-		// read response
-		try {
-			InputStream response = connection.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(response, charset));
-			StringBuilder sb = new StringBuilder();
-		    for (String line; (line = reader.readLine()) != null;) {
-		            sb.append(line);
-		    }
-			if (sb.length() > 0) {
-				id = extractId(sb.toString());
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		return id;
-	}
 	
 	
 	public static File downloadFling(int flingId, String serverBaseURL, String folderPath){
